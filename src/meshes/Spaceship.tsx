4114569060154dbe7be94618d87e003c41390908
Gaeun Yoo <gaeunyoo3626@gmail.com>
2025-01-13 09:19:04 -0500

    fix geometry does not exist issue

--- src/meshes/Spaceship.tsx
+++ src/meshes/Spaceship.tsx
@@ -1,5 +1,6 @@
 import { useGLTF } from "@react-three/drei";
 import { GroupProps } from "@react-three/fiber";
+import { Mesh } from "three";
 
 const Spaceship = ({ props } : { props?: GroupProps}) => {
   const { nodes, materials } = useGLTF('/src/assets/space_shuttle/scene.gltf')
@@ -8,7 +9,7 @@ const Spaceship = ({ props } : { props?: GroupProps}) => {
       <mesh
         castShadow
         receiveShadow
-        geometry={nodes.Cube_Material_0.geometry}
+        geometry={(nodes.Cube_Material_0 as Mesh).geometry}
         material={materials.Material}
         rotation={[-Math.PI / 2, 0, Math.PI / 2]}
         scale={100}
